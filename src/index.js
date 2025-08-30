b099bda6747f0dc7513654edc4afc1ac7a64e283
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-08-30 11:08:00 +0100

    made changes to index.js file

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5005;
 
 const allowedOrigins = [
   "http://localhost:5173", // for local development
-  "https://jobconnect-beige.vercel.app",
+  "https://jobconnect-beige.vercel.app/",
 ];
 
 app.use(
