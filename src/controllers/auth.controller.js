d307693fa2ae487171b898a1995437009a17b5b1
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-08-30 11:26:09 +0100

    set the same site key to none

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -131,7 +131,7 @@ export const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
   });
   res.status(200).json({ success: true, message: "Logged out successfully" });
 };
