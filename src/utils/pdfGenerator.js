89182f00c5490225d1d970b76e682f4a3ce2e6c4
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-09-23 09:49:21 +0100

    added apt.txt file and edited the pdfgenerator utility file to allow the export to pdf functionality work in deployment

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -1,7 +1,19 @@
 import puppeteer from "puppeteer";
 
 export const generatePDF = async (html) => {
-  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new", // or true
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu",
+    ],
+  });
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
   const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
