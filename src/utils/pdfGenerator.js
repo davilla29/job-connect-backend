731a293bb5b9a7ad46b7e4a7c05a10203d68dbf7
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-09-23 10:30:07 +0100

    changed the pdf exporter library to playwright

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -21,32 +21,32 @@
 //   return pdfBuffer;
 // };
 
-import puppeteer from "puppeteer";
+// src/utils/pdfGenerator.js
+import { chromium } from "playwright";
+let browser;
 
 export const generatePDF = async (html) => {
   try {
-    const browser = await puppeteer.launch({
-      headless: "new", // true also works
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-accelerated-2d-canvas",
-        "--no-first-run",
-        "--no-zygote",
-        "--disable-gpu",
-      ],
+    browser = await chromium.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
-
     const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: "networkidle0" });
 
-    const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
+    await page.setContent(html, { waitUntil: "networkidle" });
+
+    const pdfBuffer = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      preferCSSPageSize: true, //needed for consistent stylng
+    });
 
     await browser.close();
     return pdfBuffer;
   } catch (err) {
-    console.error("Puppeteer PDF generation failed:", err);
+    console.error("Playwright PDF generation failed:", err);
     throw err;
+  } finally {
+    if (browser) await browser.close();
   }
 };
