6b09441459cf2fee14b4853a49190412318c0594
Bolarinwa David <bolarinwadavid3@gmail.com>
2026-04-18 07:13:06 +0100

    add mailjet config file

--- src/mail/emailService.js
+++ src/mail/emailService.js
@@ -1,86 +1,3 @@
-// import { transporter } from "./smtp.config.js";
-// import {
-//   VERIFICATION_EMAIL_TEMPLATE,
-//   WELCOME_EMAIL_TEMPLATE,
-//   PASSWORD_RESET_REQUEST_TEMPLATE,
-//   PASSWORD_RESET_SUCCESS_TEMPLATE,
-// } from "./emailTemplates.js";
-
-// const senderEmail = '"Bolarinwa David" <bolarinwadavid3@gmail.com>';
-
-// export const sendVerificationEmail = async (email, name, verificationToken) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: senderEmail,
-//       to: email,
-//       subject: "Verify your email",
-//       html: VERIFICATION_EMAIL_TEMPLATE.replace("{userName}", name).replace(
-//         "{verificationCode}",
-//         verificationToken
-//       ),
-//     });
-
-//     console.log("Verification email sent:", info.messageId);
-//   } catch (error) {
-//     console.error("Failed to send verification email", error);
-//     throw new Error(`Verification email error: ${error}`);
-//   }
-// };
-
-// export const sendWelcomeEmail = async (email, name) => {
-//   try {
-//     const html = WELCOME_EMAIL_TEMPLATE.replace("{userName}", name);
-
-//     const info = await transporter.sendMail({
-//       from: senderEmail,
-//       to: email,
-//       subject: "Welcome!",
-//       html,
-//     });
-
-//     console.log("Welcome email sent:", info.messageId);
-//   } catch (error) {
-//     throw new Error(`Welcome email error: ${error}`);
-//   }
-// };
-
-// export const sendPasswordResetEmail = async (email, resetURL) => {
-//   try {
-//     const html = PASSWORD_RESET_REQUEST_TEMPLATE.replace(
-//       "{resetURL}",
-//       resetURL
-//     );
-
-//     const info = await transporter.sendMail({
-//       from: senderEmail,
-//       to: email,
-//       subject: "Reset Your Password",
-//       html,
-//     });
-
-//     console.log("Password reset email sent:", info.messageId);
-//   } catch (error) {
-//     throw new Error(`Password reset email error: ${error}`);
-//   }
-// };
-
-// export const sendResetSuccessEmail = async (email) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: senderEmail,
-//       to: email,
-//       subject: "Password Reset Successful",
-//       html: PASSWORD_RESET_SUCCESS_TEMPLATE,
-//     });
-
-//     console.log("Password reset success email sent:", info.messageId);
-//   } catch (error) {
-//     throw new Error(`Reset success email error: ${error}`);
-//   }
-// };
-
-
-
 import sgMail from "./sendgrid.config.js";
 import {
   VERIFICATION_EMAIL_TEMPLATE,
@@ -102,7 +19,7 @@ export const sendVerificationEmail = async (email, name, verificationToken) => {
       subject: "Verify your email",
       html: VERIFICATION_EMAIL_TEMPLATE.replace("{userName}", name).replace(
         "{verificationCode}",
-        verificationToken
+        verificationToken,
       ),
     };
 
@@ -111,7 +28,7 @@ export const sendVerificationEmail = async (email, name, verificationToken) => {
   } catch (error) {
     console.error(
       "❌ Failed to send verification email",
-      error.response?.body || error
+      error.response?.body || error,
     );
     throw new Error("Verification email error");
   }
@@ -154,7 +71,7 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
   } catch (error) {
     console.error(
       "❌ Password reset email error",
-      error.response?.body || error
+      error.response?.body || error,
     );
     throw new Error("Password reset email error");
   }
@@ -177,7 +94,7 @@ export const sendResetSuccessEmail = async (email) => {
   } catch (error) {
     console.error(
       "❌ Reset success email error",
-      error.response?.body || error
+      error.response?.body || error,
     );
     throw new Error("Reset success email error");
   }
---
+++ src/mail/mailjet.config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import Mailjet from "node-mailjet";
+
+export const mailjet = Mailjet.apiConnect(
+  process.env.MAILJET_API_KEY,
+  process.env.MAILJET_SECRET_KEY,
+);
